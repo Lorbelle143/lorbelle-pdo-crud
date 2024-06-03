357e31f8915f9fa85932ad6fb3f9e8d715e3e2ff
lobrele <lorbelleganzan@gmail.com>
2024-06-03 12:31:53 +0800

    update my sql

--- admin/db/lobrele.sql
+++ admin/db/lobrele.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 30, 2024 at 08:53 AM
+-- Generation Time: May 30, 2024 at 04:08 PM
 -- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -34,14 +34,10 @@ CREATE TABLE `addresses` (
   `state` varchar(100) NOT NULL,
   `postal_code` varchar(20) NOT NULL,
   `country` varchar(100) NOT NULL,
+  `payment_id` int(6) UNSIGNED NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `payments`
---
 
 CREATE TABLE `payments` (
   `id` int(6) UNSIGNED NOT NULL,
@@ -51,19 +47,7 @@ CREATE TABLE `payments` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `payments`
---
 
-INSERT INTO `payments` (`id`, `product_name`, `price`, `payment_method`, `created_at`) VALUES
-(8, '', 0.00, 'PayMaya', '2024-05-30 06:51:09'),
-(9, '', 0.00, 'PayMaya', '2024-05-30 06:51:11');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `products`
---
 
 CREATE TABLE `products` (
   `id` int(11) NOT NULL,
@@ -81,11 +65,11 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `title`, `description`, `price`, `rrp`, `quantity`, `img`, `date_added`) VALUES
-(18, 'Chess Set with Staunton Pieces', 'A classic chess set featuring Staunton-style pieces, the standard design used in tournament play.', 600, 1000, 50, 'https://i.pinimg.com/236x/40/75/f5/4075f58f0c4f1921a4fe805ea1383632.jpg', '2024-05-30 14:20:12'),
-(19, 'Chess Clock:', 'Essential for competitive play, a chess clock is used to time each player\'s moves, ensuring fair gameplay and preventing one player from taking too long.', 1000, 1500, 50, 'https://i.pinimg.com/236x/65/78/85/657885667ded2cb416bdc05b36c1eb57.jpg', '2024-05-30 14:22:51'),
-(20, 'Chess Strategy Books', 'Countless books are available on chess strategy, ranging from beginner guides to advanced tactics.', 2000, 1500, 50, 'https://i.pinimg.com/736x/2c/f6/2a/2cf62abd08a385df200a0bb4da08980c.jpg', '2024-05-30 14:29:43'),
-(21, 'Chess Software and Apps', 'Chess software and apps provide a convenient way to practice and improve your skills.', 2000, 1500, 50, 'https://i.pinimg.com/564x/56/f3/b7/56f3b747095287c00490bcc19f5b6fbf.jpg', '2024-05-30 14:36:44'),
-(22, 'Chess-themed Merchandise', 'From t-shirts and mugs to posters and novelty items, there\'s a plethora of chess-themed merchandise available for enthusiasts', 5000, 4500, 100, 'https://i.pinimg.com/736x/c3/c7/a7/c3c7a7cd29d07034b199f48f9c738e38.jpg', '2024-05-30 14:40:38');
+(1, 'Chess Set with Staunton Pieces', 'A classic chess set featuring Staunton-style pieces, the standard design used in tournament play.', 600, 1000, 50, 'https://i.pinimg.com/236x/40/75/f5/4075f58f0c4f1921a4fe805ea1383632.jpg', '2024-05-30 14:20:12'),
+(2, 'Chess Clock:', 'Essential for competitive play, a chess clock is used to time each player\'s moves, ensuring fair gameplay and preventing one player from taking too long.', 1000, 1500, 50, 'https://i.pinimg.com/236x/65/78/85/657885667ded2cb416bdc05b36c1eb57.jpg', '2024-05-30 14:22:51'),
+(3, 'Chess Strategy Books', 'Countless books are available on chess strategy, ranging from beginner guides to advanced tactics.', 2000, 1500, 50, 'https://i.pinimg.com/736x/2c/f6/2a/2cf62abd08a385df200a0bb4da08980c.jpg', '2024-05-30 14:29:43'),
+(4, 'Chess Software and Apps', 'Chess software and apps provide a convenient way to practice and improve your skills.', 2000, 1500, 50, 'https://i.pinimg.com/564x/56/f3/b7/56f3b747095287c00490bcc19f5b6fbf.jpg', '2024-05-30 14:36:44'),
+(5, 'Chess-themed Merchandise', 'From t-shirts and mugs to posters and novelty items, there\'s a plethora of chess-themed merchandise available for enthusiasts', 5000, 4500, 100, 'https://i.pinimg.com/736x/c3/c7/a7/c3c7a7cd29d07034b199f48f9c738e38.jpg', '2024-05-30 14:40:38');
 
 -- --------------------------------------------------------
 
@@ -105,7 +89,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `created_at`) VALUES
-(5, 'lobrele', '$2y$10$ypiBOYHF41r12hRt2XGTcu.JbwZuRb79emfKsOjkiLQJJ3fnX.H5.', '2024-05-30 14:13:44');
+(1, 'admin', '$2y$10$kGp4g1TjBK4XwLIwRbBHSeZ4W5FpPbYoB1ap5NfFUjUPAcE3KR5QG', '2024-04-29 16:39:58'),
+(2, 'lobrele', '$2y$10$26m/9gmMDpV4k0Y9UXLHI.TnvIRgoyNdkeQ/NC.XN6v3Q5ZgMz0dO', '2024-05-29 09:12:28');
 
 --
 -- Indexes for dumped tables
@@ -115,24 +100,13 @@ INSERT INTO `users` (`id`, `username`, `password`, `created_at`) VALUES
 -- Indexes for table `addresses`
 --
 ALTER TABLE `addresses`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `payment_id` (`payment_id`);
 
 --
 -- Indexes for table `payments`
 --
 ALTER TABLE `payments`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `products`
---
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -143,25 +117,23 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `addresses`
 --
 ALTER TABLE `addresses`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `payments`
 --
 ALTER TABLE `payments`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- AUTO_INCREMENT for table `products`
+-- Constraints for dumped tables
 --
-ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
--- AUTO_INCREMENT for table `users`
+-- Constraints for table `addresses`
 --
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `addresses`
+  ADD CONSTRAINT `addresses_ibfk_1` FOREIGN KEY (`payment_id`) REFERENCES `payments` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
